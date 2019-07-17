82e93dba4dba70b031364e876640ef9a3ae44b62
Nicolas PASCAL <np.pascal@gmail.com>
2019-07-17 17:38:44 +0200

    Update test_docker.sh

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -16,7 +16,7 @@ cd my-shop
 
 # run the project's tests
 #docker-compose -f up --build -d
-
+docker-compose run ls -l
 docker-compose run python manage.py pytest
 
 # return non-zero status code if there are migrations that have not been created
