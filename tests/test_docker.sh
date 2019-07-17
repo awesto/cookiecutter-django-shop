95e312565cb46e39c5bd55235f73e87612a95beb
Nicolas PASCAL <np.pascal@gmail.com>
2019-07-17 13:34:00 +0200

    Update test_docker.sh

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -5,7 +5,7 @@
 
 # install test requirements
 #pip install -r requirements.txt
-poetry install 
+#poetry install 
 # create a cache directory
 mkdir -p .cache/docker
 cd .cache/docker
@@ -16,9 +16,9 @@ cookiecutter ../../ --no-input --overwrite-if-exists use_docker=y
 cd my-shop
 ls
 # run the project's tests
-docker-compose -f up --build -d
+#docker-compose -f up --build -d
 
-#docker-compose -f local.yml run django python manage.py pytest
+docker-compose -f docker-compose.yml run django python manage.py pytest
 
 # return non-zero status code if there are migrations that have not been created
 #docker-compose -f local.yml run django python manage.py makemigrations --dry-run --check || { echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"; exit 1; }
