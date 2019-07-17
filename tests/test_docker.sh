91fc151a9f0e49a35acc9d10f7ef5cf943da0def
Nicolas PASCAL <np.pascal@gmail.com>
2019-07-17 21:38:30 +0200

    Update test_docker.sh

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -16,6 +16,8 @@ cd my-shop
 # run the project's tests
 docker-compose -f up --build -d
 docker-compose up
+#list images 
+docker-compose images  
 docker-compose exec web /bin/bash
 docker-compose exec web /bin/sh ls
 docker-compose exec web /bin/sh poetry shell &&
