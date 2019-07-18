64c7f90f5997832ee0150fc2b74b059ba4cf9126
Nicolas PASCAL <np.pascal@gmail.com>
2019-07-18 05:26:47 +0200

    Update test_docker.sh

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -12,25 +12,17 @@ cd .cache/docker
 
 # create the project using the default settings in cookiecutter.json
 cookiecutter ../../  --no-input  --overwrite-if-exists  dockerize="runserver" debug="y"
-ls
+
 cd my-shop
 ls
-# run the project's tests
- ##docker-compose build shop/hub
-#docker build -t carlad/sinatra .
-#docker-compose build -t . web
-docker-compose -f up --build -d
 
-
-
-#docker-compose exec web /bin/bash poetry shell &&
-# docker-compose run /bin/bash python
 #docker-compose run python manage.py pytest
+docker-compose -f docker-compose.yml run django python manage.py pytest
 
 # return non-zero status code if there are migrations that have not been created
-#docker-compose run python manage.py makemigrations --dry-run --check || { echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"; exit 1; }
+docker-compose -f docker-compose.yml run django python manage.py makemigrations --dry-run --check || { echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"; exit 1; }
 
 # Test support for translations
-#docker-compose  run python manage.py makemessages
+docker-compose -f docker-compose.yml run django python manage.py makemessages
 
 exit 0
