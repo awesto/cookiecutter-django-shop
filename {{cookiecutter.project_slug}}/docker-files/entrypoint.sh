578d98ff13864f4ac139fbb540a6fe37884c8b42
Nicolas PASCAL <np.pascal@gmail.com>
2019-07-19 20:06:52 +0200

    Update entrypoint.sh

--- {{cookiecutter.project_slug}}/docker-files/entrypoint.sh
+++ {{cookiecutter.project_slug}}/docker-files/entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 /web/manage.py initialize_shop_demo --noinput
 exec /web/manage.py makemigrations
+exec /web/manage.py migrate
 exec /web/manage.py check
 #exec /web/manage.py runserver 0.0.0.0:9009 
