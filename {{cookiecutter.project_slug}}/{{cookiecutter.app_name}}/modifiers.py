49bfb48f562718fe2bf5846e5975d3bc2fb24ac8
Jacob Rief <jacob.rief@gmail.com>
2019-10-21 18:47:42 +0200

    Fix is_active has been called with wrong type

--- {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/modifiers.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/modifiers.py
@@ -37,7 +37,7 @@ class PostalShippingModifier(ShippingModifier):
         return (self.identifier, _("Postal shipping"))
 
     def add_extra_cart_row(self, cart, request):
-        if not self.is_active(cart) and len(cart_modifiers_pool.get_shipping_modifiers()) > 1:
+        if not self.is_active(cart.extra.get('shipping_modifier')) and len(cart_modifiers_pool.get_shipping_modifiers()) > 1:
             return
         # add a shipping flat fee
         amount = Money('5')
