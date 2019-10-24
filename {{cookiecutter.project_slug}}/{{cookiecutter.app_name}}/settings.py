a5958b7c23ab731a7239d99ee5857d488de58ec0
Jacob Rief <jacob.rief@gmail.com>
2019-10-25 00:35:42 +0200

    django messages are required by Django-2.2

--- {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/settings.py
@@ -82,6 +82,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
     'djangocms_text_ckeditor',
