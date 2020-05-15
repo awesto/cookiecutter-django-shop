9073070b527301e6dc681337ddbfd5f6ac15e8b9
Jacob Rief <jacob.rief@gmail.com>
2020-05-16 00:39:08 +0200

    make search indexes also for I18N = False

--- {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/search_indexes.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/search_indexes.py
@@ -1,10 +1,15 @@
-from django.conf import settings
-
+{% if cookiecutter.use_i18n == 'y' -%}
+from django.conf import settings as config
+{% endif %}
 from shop.search.documents import ProductDocument
 
+settings = {
+    'number_of_shards': 1,
+    'number_of_replicas': 0,
+}
+{%- if cookiecutter.use_i18n == 'y' %}
 for language, _ in settings.LANGUAGES:
-    settings = {
-        'number_of_shards': 1,
-        'number_of_replicas': 0,
-    }
-    ProductDocument(language, settings)
+    ProductDocument(language=language, settings=settings)
+{% else %}
+ProductDocument(settings=settings)
+{% endif -%}
