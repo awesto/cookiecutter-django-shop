4f8f3fc12913966b1a33aa52532864a668c00ccb
Jacob Rief <jacob.rief@gmail.com>
2020-05-23 21:04:16 +0200

    make search apphook optional

--- {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/cms_apps.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/cms_apps.py
@@ -11,14 +11,16 @@ from shop.rest.filters import CMSPagesFilterBackend
 
 class CatalogListApp(CatalogListCMSApp):
     def get_urls(self, page=None, language=None, **kwargs):
-{%- if cookiecutter.products_model == 'polymorphic' %}
+{%- if cookiecutter.use_elasticsearch == 'y' %}
         from shop.search.mixins import ProductSearchViewMixin
-        from shop.views.catalog import AddToCartView, AddFilterContextMixin, ProductListView, ProductRetrieveView
+{%- endif %}
+        from shop.views.catalog import AddToCartView, ProductListView, ProductRetrieveView
+{%- if cookiecutter.products_model == 'polymorphic' %}
+        from shop.views.catalog import AddFilterContextMixin
         from {{ cookiecutter.app_name }}.filters import ManufacturerFilterSet
         from {{ cookiecutter.app_name }}.serializers import AddSmartPhoneToCartSerializer
 
-        bases = (AddFilterContextMixin, ProductSearchViewMixin, ProductListView)
-        ProductSearchListView = type('SearchView', bases, {})
+        ProductListView = type('ProductSearchListView', (AddFilterContextMixin, {% if cookiecutter.use_elasticsearch == 'y' %}ProductSearchViewMixin, {% endif %}ProductListView), {})
         filter_backends = [CMSPagesFilterBackend]
         filter_backends.extend(api_settings.DEFAULT_FILTER_BACKENDS)
         return [
@@ -29,15 +31,16 @@ class CatalogListApp(CatalogListCMSApp):
             url(r'^(?P<slug>[\w-]+)', ProductRetrieveView.as_view(
                 use_modal_dialog=False,
             )),
-            url(r'^', ProductSearchListView.as_view(
+            url(r'^', ProductListView.as_view(
                 filter_backends=filter_backends,
                 filter_class=ManufacturerFilterSet,
             )),
         ]
 {%- else %}{% set use_lookup_field = (cookiecutter.products_model == 'commodity' and cookiecutter.use_i18n == 'y') %}
-        from shop.views.catalog import AddToCartView, ProductListView, ProductRetrieveView
         from {{ cookiecutter.app_name }}.serializers import ProductDetailSerializer
-
+    {% if cookiecutter.use_elasticsearch == 'y' %}
+        ProductListView = type('ProductSearchListView', (ProductSearchViewMixin, ProductListView), {})
+    {%- endif %}
         return [
             url(r'^(?P<slug>[\w-]+)/add-to-cart', AddToCartView.as_view({% if use_lookup_field %}lookup_field='translations__slug'{% endif %})),
             url(r'^(?P<slug>[\w-]+)', ProductRetrieveView.as_view(
@@ -54,7 +57,12 @@ class CatalogListApp(CatalogListCMSApp):
 
 apphook_pool.register(CatalogListApp)
 
+{%- if cookiecutter.use_elasticsearch == 'y' %}
+
+from shop.cms_apphooks import CatalogSearchApp
+
 apphook_pool.register(CatalogSearchApp)
+{%- endif %}
 
 apphook_pool.register(OrderApp)
 
